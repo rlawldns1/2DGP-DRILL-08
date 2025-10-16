0b57a88f64a713424f6ac6ea69a63e23398544f4
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-10-16 17:45:02 +0900

    a_up 삭제

--- boy.py
+++ boy.py
@@ -21,8 +21,6 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
-def a_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_a
 
 
 class AutoRun:
