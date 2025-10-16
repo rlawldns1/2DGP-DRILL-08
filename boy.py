4d7aa4c31fb317ecf6504b9e7a40ca7365709e9c
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-10-16 17:44:33 +0900

    autorun에서 a를 누르면 idle로 변경되는 것 삭제

--- boy.py
+++ boy.py
@@ -146,7 +146,7 @@ class Boy:
                 self.SLEEP: {space_down: self.IDLE, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
                 self.IDLE: {time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN, a_down: self.AUTO_RUN},
                 self.RUN: {right_down: self.IDLE, left_down : self.IDLE, right_up: self.IDLE, left_up: self.IDLE},
-                self.AUTO_RUN: {time_out: self.IDLE, a_down: self.IDLE, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
+                self.AUTO_RUN: {time_out: self.IDLE, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
             }
         )
 
